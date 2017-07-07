036fcef255cf44f742b7b9980de088d025077142
mixelpixel <mixelpix@mac.com>
2017-07-07 12:09:14 -0400

    day1

---
+++ Day1/closure.js
@@ -0,0 +1,26 @@
+// Functions can return functions.
+
+const outer = () => { // <---- outer is a higher order function; it returns another function
+  const message = 'hi'
+  return () => {
+    console.log(message); // CLOSURE - the inner scope can look back at it's parents scope
+  };
+};
+
+const inner = outer();
+
+console.log(inner); // ---> [Function] // "outer()" is "inner()"s parent
+
+const message = 'sup'; // <------ Unnoticed by the function
+inner(); // ---> hi
+
+// use case: private variables (private, i.e. isolated), decorating functions, etc.
+
+
+// Cache - Memcache: layer between server and DB
+const cache = (cb) => {
+  const cache = {};
+  return () => {
+    cb();
+  };
+}
