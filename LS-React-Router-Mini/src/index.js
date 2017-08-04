0002e21a622021a628c895ee17adb9da1afb7d09
mixelpixel <mixelpix@mac.com>
2017-08-04 15:43:42 -0400

    React Router Mini √

--- LS-React-Router-Mini/src/index.js
+++ LS-React-Router-Mini/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+
 ReactDOM.render(
   <Router>
     <App/>
   </Router>,
   document.getElementById('root'));
-
-
-registerServiceWorker();
