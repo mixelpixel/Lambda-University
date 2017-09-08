8b980de70bf311b7e411cfcefa8484a6fb01c755
mixelpixel <mixelpix@mac.com>
2017-09-08 08:50:55 -0400

    LS-Mobile-I

--- Mobile-I/patrick/ls-mobile-1/App.js
+++ Mobile-I/patrick/ls-mobile-1/App.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() { // not using right now, but just like React
+    super();
+    this.state = {
+
+    };
+  }
   render() {
-    return (
+    console.log('Well, hello there!')
+    return ( // the JSX is different than in React
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>This is totally cool!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text>
+        <Text>This is totally cool!</Text> */}
+        <View style={styles.child}>
+          <Text style={styles.text1}>
+            First
+          </Text>
+        </View>
+        <View style={styles.child}>
+          <Text style={styles.text2}>
+            Second
+          </Text>
+        </View>
+        <View style={styles.child}>
+          <Text style={styles.text3}>
+            Third
+          </Text>
+        </View>
       </View>
     );
   }
@@ -15,8 +37,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: 'flex',
     backgroundColor: 'whitesmoke',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  child: {
+    height: 50,
+    width: 150,
+    marginBottom: 10,
+    backgroundColor: 'salmon',
+    // alignItems: 'center',
+    justifyContent: 'center', // <~~~~ that's how.
+  },
+  text1: {
+    // alignSelf: 'center',
+    textAlign: 'center',
+    // textAlignVertical: 'center', // <~~~~ HOW?????
+  },
+  text2: {
+    textAlign: 'auto',
+  },
+  text3: {
+    textAlign: 'justify',
+  }
 });
