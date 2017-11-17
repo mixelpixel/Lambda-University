ee43698ab3cfc79ad8898f9b0c00756d001fa08b
mixelpixel <mixelpix@mac.com>
2017-11-17 13:29:48 -0500

    whiteboar interview prep

--- interviewPrep/whiteboard/20171117fri.js
+++ interviewPrep/whiteboard/20171117fri.js
@@ -33,56 +33,78 @@
 
  // Stack1 and Stack2
 
- class Queue {
-   constructor () {
-     this.stack1 = [];
-     this.stack2 = [];
-   }
-
-   enqueue (item){
-     this.stack1.push(item);
-   }
-
-   dequeue (){
-     if (this.stack2.length === 0) {
-       const len = this.stack1.length;
-       for (let i = 0; i < len; i++) {
-         this.stack2.push(this.stack1.pop());
-       }
-     }
-     return this.stack2.pop();
-   }
+class Queue {
+ constructor () {
+   this.stack1 = [];
+   this.stack2 = [];
  }
 
+ enqueue (item){
+   this.stack1.push(item);
+ }
 
- // TEST SUITE
- const x = new Queue();
- x.enqueue('a');
- x.enqueue('f');
- x.enqueue('q');
- x.enqueue('m');
- x.enqueue('n');
- console.log(x);
- console.log(x.dequeue());
- console.log(x.dequeue());
- console.log(x);
- console.log(x.enqueue('sean is amazing!'));
- console.log(x);
- console.log(x.dequeue());
- console.log(x.dequeue());
- console.log(x.enqueue('Antonio is amazing!'));
- console.log(x.enqueue('Lois is amazing!'));
- console.log(x.enqueue('Tyge is amazing!'));
- console.log(x.enqueue('Manisha is amazing!'));
- console.log(x.enqueue('Sarah is amazing!'));
- console.log(x.enqueue('Dan is amazing!'));
- console.log(x.enqueue('Jake is amazing!'));
- console.log(x.enqueue('Evan is amazing!'));
- console.log(x.dequeue());
- console.log(x.dequeue());
- console.log(x);
+ dequeue (){
+   if (this.stack2.length === 0) {
+     const len = this.stack1.length;
+     for (let i = 0; i < len; i++) {
+       this.stack2.push(this.stack1.pop());
+     }
+   }
+   return this.stack2.pop();
+ }
+}
+
+
+// TEST SUITE
+const x = new Queue();
+x.enqueue('a');
+x.enqueue('f');
+x.enqueue('q');
+x.enqueue('m');
+x.enqueue('n');
+console.log(x);
+console.log(x.dequeue());
+console.log(x.dequeue());
+console.log(x);
+console.log(x.enqueue('sean is amazing!'));
+console.log(x);
+console.log(x.dequeue());
+console.log(x.dequeue());
+console.log(x.enqueue('Antonio is amazing!'));
+console.log(x.enqueue('Lois is amazing!'));
+console.log(x.enqueue('Tyge is amazing!'));
+console.log(x.enqueue('Manisha is amazing!'));
+console.log(x.enqueue('Sarah is amazing!'));
+console.log(x.enqueue('Dan is amazing!'));
+console.log(x.enqueue('Jake is amazing!'));
+console.log(x.enqueue('Evan is amazing!'));
+console.log(x.dequeue());
+console.log(x.dequeue());
+console.log(x);
 
 
 /*
- * 3) w/Ely
+ * 3) w/Ely Single Stack
  */
+ // queue with one stack
+ // FIFO
+ // PUSH , POP
+
+ class Queue {
+   constructor() {
+     this.stack = [];
+   }
+
+   enqueue (item) {
+     this.stack.push(item);
+   }
+
+   dequeue() {
+    const top = this.stack.pop();
+    if (this.stack.length === 0) return top;
+    const result = this.dequeue();
+    this.stack.push(top);
+    return result;
+   }
+ }
+ 
