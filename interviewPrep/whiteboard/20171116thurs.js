a3548d8ea06d982f6580d53c602dc5a0284f2e31
mixelpixel <mixelpix@mac.com>
2017-11-17 12:17:15 -0500

    whiteboard interview prep

--- interviewPrep/whiteboard/20171116thurs.js
+++ interviewPrep/whiteboard/20171116thurs.js
@@ -5,7 +5,22 @@
 /*
  * 1) String Compression w/Manisha
  */
-
+ function stringCompression(string) {
+   let currentChar = null;
+   let compressedStr = '';
+   let charCount = 1;
+   for (let i = 0; i <= string.length; i++) {
+     if (string[i] === currentChar) charCount++;
+     if (currentChar === null) currentChar = string[i];
+     if (string[i] !== currentChar || string[i] === undefined) {
+       compressedStr += currentChar;
+       compressedStr += charCount;
+       charCount = 1;
+       currentChar = string[i];
+     }
+   }
+   return compressedStr.length < string.length? compressedStr : string;
+ }
 
 /*
  * 2) w/Lois Balanced Brackets
@@ -30,3 +45,40 @@ function balancedBrackets(str) {
 /*
  * 3) Telephone Words w/Wesley
  */
+ // Given a string of four digits, generate all the possible four-letter combinations.
+
+function genCombs(digits) {
+  const phoneDigitsToLetters = {
+    0: '0',
+    1: '1',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ'
+  };
+
+  let answer = [];
+
+  const innerFunc = (currentWord, rest) => {
+    if (rest.length === 0) {
+      answer.push(currentWord);
+      return;
+    }
+    const currentDigit = rest[0];
+    const letters = phoneDigitsToLetters[currentDigit];
+    for (let i = 0; i < letters.length; i++) {
+      letter = letters[i];
+      innerFunc(currentWord + letter, rest.slice(1));
+    }
+  }
+
+  innerFunc('', digits);
+  return answer;
+
+}
+
+console.log(genCombs('2345'));
---
+++ interviewPrep/whiteboard/20171117fri.js
@@ -0,0 +1,17 @@
+//
+//
+/* eslint no-console: 0 */
+
+/*
+ * 1) w/Antonio Find Corresponding Closing Parenthesis
+ */
+// "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
+
+/*
+ * 2) w/
+ */
+
+
+/*
+ * 3) w/
+ */
